271ea17067c7f7b73da77caf7878df22dfbb8c2c
Ilya <i_k0stin@mail.ru>
2020-10-06 12:29:06 +0300

    second commit

--- js/script.js
+++ js/script.js
@@ -10,12 +10,32 @@ const personalMoviedatabse = {
    privat: false 
 };
 
-const a = prompt('One of the last watched films?', ''),
-      b = prompt('raiting of film', ''),
-      c = prompt('One of the last watched films?', ''),
-      d = prompt('raiting of film', '');
+for (let i = 0; i < 2; i++) {
+   const a = prompt('One of the last watched films?', ''),
+         b = prompt('raiting of film', '');
 
-personalMoviedatabse.movies[a] = b;
-personalMoviedatabse.movies[c] = d;
+   if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+
+      personalMoviedatabse.movies[a] = b;
+      console.log('done');
+   } else {
+      console.log('error');
+      i--;
+   }
+}
+
+if (personalMoviedatabse.count < 10) {
+   console.log('Мало фильмов');
+} else if (personalMoviedatabse.count >= 10 && personalMoviedatabse.count < 30 ) {
+   console.log('Классический зритель');
+} else if (personalMoviedatabse.count >= 30) {
+   console.log('Киноман');
+} else {
+   console.log('Ошибка');
+}
+
+
 
 console.log(personalMoviedatabse);
+
+
